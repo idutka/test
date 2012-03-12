44846cb2c17d01a0a152cb175a39cfca2dbfde8e
vanok <vanok@comp>
2012-03-12 19:45:46 +0200

    first comit

---
+++ faktorial/src/faktorial.cpp
@@ -0,0 +1,23 @@
+//============================================================================
+// Name        : faktorial.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n,i;
+	int f=1;
+	cout << "Введіть число: ";
+	cin >> n;
+	for(i=1;i<=n;i++)
+	{
+		f*=i;
+	}
+	cout << "Факторіал числа " << n << " рівний: " << f;
+	return 0;
+}
