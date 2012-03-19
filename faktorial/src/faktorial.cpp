e825fea88e7f0eb0912663001945dfcd5565556c
vanok <vanok@comp>
2012-03-19 14:10:55 +0200

    111

--- faktorial/src/faktorial.cpp
+++ faktorial/src/faktorial.cpp
@@ -1,6 +1,6 @@
 //============================================================================
 // Name        : faktorial.cpp
-// Author      : 
+// Author      : ----
 // Version     :  20:02
 // Copyright   : Your copyright notice
 // Description : Hello World in C++, Ansi-style
